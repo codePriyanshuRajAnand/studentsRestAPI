eac612bc11700a6c4a7d864e1dd2beb9774e0746
codePriyanshuRajAnand <priyanshurajanand2@gmail.com>
2025-12-21 18:49:08 +0530

    Searlized the Request/Response

--- internal/http/handlers/students/student.go
+++ internal/http/handlers/students/student.go
@@ -1,9 +1,26 @@
 package students
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/codePriyanshuRajAnand/studentsRestAPI/internal/types"
+	"github.com/codePriyanshuRajAnand/studentsRestAPI/internal/utils/response"
+)
 
 func Create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Students Rest API"))
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.ErrorWriter(err))
+			return
+		}
+		slog.Info("Creating new student...")
+		// w.Write([]byte("Welcome to Students Rest API"))
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
 }
