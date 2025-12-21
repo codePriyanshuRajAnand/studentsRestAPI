55658b1a5f8da1e14b09b3803b6fd963d48c9aa0
codePriyanshuRajAnand <priyanshurajanand2@gmail.com>
2025-12-21 19:17:38 +0530

    Added user input Vaidator

--- internal/http/handlers/students/student.go
+++ internal/http/handlers/students/student.go
@@ -3,23 +3,36 @@ package students
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 
 	"github.com/codePriyanshuRajAnand/studentsRestAPI/internal/types"
 	"github.com/codePriyanshuRajAnand/studentsRestAPI/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func Create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var student types.Student
 		err := json.NewDecoder(r.Body).Decode(&student)
+		slog.Info("Creating new student...")
 		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.ErrorWriter(fmt.Errorf("request body is empty")))
+			return
+		}
+		if err != nil {
 			response.WriteJson(w, http.StatusBadRequest, response.ErrorWriter(err))
 			return
 		}
-		slog.Info("Creating new student...")
+		//request validation
+		if err := validator.New().Struct(student); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErr))
+			return
+		}
+
 		// w.Write([]byte("Welcome to Students Rest API"))
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
